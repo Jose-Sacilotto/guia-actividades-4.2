9c07f8876f512c96d0ea3610ea43f8c83773b0d8
jose-sacilotto <joseignaciosacilotto@gmail.com>
2024-09-17 08:22:19 -0300

    ejercicio 27

---
+++ ejercicio 27.py
@@ -0,0 +1,8 @@
+#Leer dos números e imprimir el mayor, suponer que son distintos.
+numero1=int(input("Ingresa el primer numero: "))
+numero2=int(input("Ingresa el segundo numero: "))
+
+if numero1>numero2:
+    print(f"El numero {numero1} es mayor")
+else:
+    print(f"El numero {numero2} es mayor")
