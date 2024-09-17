0915456d8dd8fd3ba60f1f41413ba148b0f2fecc
jose-sacilotto <joseignaciosacilotto@gmail.com>
2024-09-17 07:48:38 -0300

    ejercicio 24

--- ejercicio 24.py
+++ ejercicio 24.py
@@ -1,9 +1,6 @@
 #Indicar si el número cinco es par o impar.
-numero=int(input("Ingrese un nùmero:"))
-def es_par(numero):
-    return numero %2==0
-if es_par(numero):
-    print(f"{numero} es par")
+numero=5
+if numero %2==0:
+    print(f"El número {numero} es par")
 else:
-    print(f"{numero} es impar")
-    
+    print(f"El número {numero} es impar")
